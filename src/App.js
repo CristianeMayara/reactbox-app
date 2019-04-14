d45fd61d25a3157d8dbaf889a91eaf7d961b01e0
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-14 20:09:02 -0300

    Adding the main page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
+import Main from "./pages/Main";
+
 import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div>hello world</div>
-      </div>
-    );
+    return <Main />;
   }
 }
 
